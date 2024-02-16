a7ba81f9cb8cb5f5c51384e3f2cc0c1cc3378f07
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-02-16 16:40:48 +0500

    added one question

---
+++ Arrays/Easy /Unique_No_of_Occurrence.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+bool uniqueOccurrences(int arr[], int size) {
+    int count1 = 0;
+
+    for(int i=0; i<size; i++) {
+        if(arr[i] != arr[i+1])
+            int count;
+        for(int j=i+1; j<size; j++) {
+
+        }
+    }
+}
